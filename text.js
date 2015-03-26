d5f18149cfce5c9f1f8fd8dac3ba3ff29c68feb3
Nitive <samoilowmaxim@gmail.com>
2015-03-26 14:57:29 +0600

    Delete old comments

--- text.js
+++ text.js
@@ -1,13 +1,11 @@
 function applyTextStyles(key, value) {
-  // set typeface
+
   if (key == "typeface") {
     [layer setFontPostscriptName:value]];
   }
 
-  // set weight
   if (key == "weight") {
     var newFontName = [layer fontPostscriptName].split("-")[0] + "-";
-    // capitalize value
     var weights = value.split(/\s+/));
     for (j = 0; j < weights.length; j++) {
       newFontName += weights[j].capitalize();
@@ -15,12 +13,10 @@ function applyTextStyles(key, value) {
     [layer setFontPostscriptName:newFontName];
   }
 
-  // set color
   if (key == "color") {
     [layer setTextColor:colorFromText(value)];
   }
 
-  // set size
   if (key == "size") {
     [layer setFontSize:value];
   }
