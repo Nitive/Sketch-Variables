d36228c1b2f97690974e2f66e64e59f719953b01
Nitive <samoilowmaxim@gmail.com>
2015-04-03 23:57:13 +0600

    Add getting color from three-character hex

--- utilities.js
+++ utilities.js
@@ -1,7 +1,19 @@
 String.prototype.capitalize = function() {
   return this[0].toUpperCase() + this.substring(1);
 }
+String.prototype.repeat= function(n){
+    n= n || 1;
+    return Array(n+1).join(this);
+}
+
+function lg(msg, title) {
+  title = (title || "");
+  if (title != "") title += ": ";
+  NSLog("%@%@", title, msg);
+}
 
 function colorFromText(text) {
+  if (text.match(/^#[\da-f]{3}$/i))
+    text = '#' + text[1].repeat(2) + text[2].repeat(2) + text[3].repeat(2);
   return MSColor.colorWithNSColor(NSColor.colorWithHex(text));
 }
